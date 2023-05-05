f9c5a155bb3e70dff54ceda3dfcab8b3c2207158
Elissa <elissadusabe@gmail.com>
2023-05-05 23:54:25 +0200

    js finished

--- script.js
+++ script.js
@@ -80,5 +80,19 @@ const initGame = () => {
       gameOver = true
      }
 
+     for (let i = 0; i< snakeBody.length; i++) {
+          htmlMarkup += `<div class="snake" style= "grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`  
+            if(i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0] ) {
+               gameOver = true
+            }
+     }
+   
+     playBoard.innerHTML = htmlMarkup;
+}
 
+changeFoodPosition()
+ setIntervalId =  setInterval (initGame , 125)
+
+
+document.addEventListener("keydown", changeDirection)
 
